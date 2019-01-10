2ace71bb2624f0ee2592b1e183325823cd0d66ae
stasulam <l.ambroziak@protonmail.com>
2019-01-10 22:06:00 +0100

    Installation instructions

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='faces',
+    version='0.1',
+    author='Lukasz Ambroziak',
+    description='Detect and recognize faces.',
+    url='https://github.com/stasulam/faces',
+    license='MIT',
+    packages=['faces'],
+    zip_safe=True
+)

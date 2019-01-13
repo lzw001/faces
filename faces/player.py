b4343c3c4b7dc125c324655cbe62fa2a229f126f
stasulam <l.ambroziak@protonmail.com>
2019-01-13 15:25:01 +0100

    Fix: whitespaces

--- faces/player.py
+++ faces/player.py
@@ -4,19 +4,19 @@ import vlc
 
 
 class Player(object):
-	"""Player allows to play specific playlist."""
-	def __init__(self) -> None:
+    """Player allows to play specific playlist."""
+    def __init__(self) -> None:
 
-		self.vlc = vlc.Instance()
+        self.vlc = vlc.Instance()
 
-	def play(self, url: str) -> None:
-		"""
-		"""
-		player = self.vlc.media_player_new()
-		media = self.vlc.media_new(pafy.new(url).getbest().url)
-		media.get_mrl()
-		player.set_media(media)
-		player.play()
-		time.sleep(0.1)
-		song_duration = player.get_length() / 1000
-		time.sleep(song_duration)
+    def play(self, url: str) -> None:
+        """
+        """
+        player = self.vlc.media_player_new()
+        media = self.vlc.media_new(pafy.new(url).getbest().url)
+        media.get_mrl()
+        player.set_media(media)
+        player.play()
+        time.sleep(0.1)
+        song_duration = player.get_length() / 1000
+        time.sleep(song_duration)

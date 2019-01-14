13c404c6d285bbf3cb2449bdec9e4b3ef17beac1
stasulam <l.ambroziak@protonmail.com>
2019-01-14 22:15:57 +0100

    10 consecutive predictions indicate the same person

--- faces/__main__.py
+++ faces/__main__.py
@@ -14,19 +14,23 @@ if __name__ == '__main__':
 	# define while loop to capture image from webcam
 	cv2.namedWindow('SpotiFaces')
 	camera = cv2.VideoCapture(0)
+	predictions = list()
 	while True:
 		_, image = camera.read()
 		image = Videos2Frames.preprocess_frame(image,
 			rotation=None,
 			crop=dict(left=420, right=420, bottom=120, top=120),
-			resize=(600, 600))
+			resize=(256, 256))
 		cv2.imshow('SpotiFaces', image)
 		try:
 			landmarks_dists = flt.face_landmarks_distances(image)
 			prediction = logreg.predict_proba(landmarks_dists.reshape(1, 2278))
-			print('Czesc, {}'.format(['anna', 'lukasz'][np.argmax(prediction)]))
+			predictions.append(['anna', 'lukasz'][np.argmax(prediction)])
 		except IndexError:
 			next
+		if len(predictions) == 10 and predictions.count(predictions[0]) == len(predictions):
+			print(predictions[0])
+			predictions = list()
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 	camera.release()
